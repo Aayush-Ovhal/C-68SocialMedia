1ba12c7f89a4031dfd24e33df240ba1663ae241f
Aayush-Ovhal <61664556+Aayush-Ovhal@users.noreply.github.com>
2020-10-24 13:13:14 +0530

    Add files via upload

---
+++ screens/facebook.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Text, View, Stylesheet, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants';
+
+export default class Facebook extends React.Component{
+    render(){
+        return(
+          <View>
+              <Text>Facebook</Text>
+          </View>
+        )
+    }
+}
---
+++ screens/instagram.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Text, View, Stylesheet, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants';
+
+export default class Insta extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Instagram</Text>
+            </View>
+        )
+    }
+}
